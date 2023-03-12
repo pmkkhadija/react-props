43355190e2d04bcb798cb6d22530a4b2d91535c3
khadija <pmkkhadija@gmail.com>
2023-03-12 23:34:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from "react";
-import Profilecomponent from "./profilecomponent";
-const App = () => (
- <>
-   <Profilecomponent />
- </>
- function fullname (props) {
-  return <h1>fullname:{props.name}</h1>;
- }
- const fullname = () => {
-  return (
-    <div>
-      <fullname name="maryam khadija" /> 
-      <fullname name=" isra muhammad" /> 
-    </div>
-  );
+import React from "react";
+import button from '.App.js'
+class person  extends React.Component {
+ state = {
+   fullname: "Sara johson",
+   bio: "i'm a student from havard university",
+   myimage: 'https://wallpapershome.com/images/pages/pic_h/23660.jpg',
+   profession: "marketing'assistant",
+   isEmployed: true,
+   showProfile: false ,
+   timeSinceMount: 0
+
  };
+}
+componentDidMount() {
+   this.setState({ timeSinceMount: new Date() })
+ }
+render(); {
+     <><button id="toggle-button">Toggle Profile</button><script>
+        const button = document.getElementById("toggle-button");
+        button.addEventListener("click", () => {setState(prevState => ({
+           ...prevState,
+           showProfile: !prevState.showProfile
+        }))};
+        });
+     </script></>
+}
+{state.showProfile && (
+   <div>
+      <h2>{state.fullname}</h2>
+      <p>{state.bio}</p>
+      <img src={state.myimage} alt="Profile Image" />
+      <p>Profession: {state.profession}</p>
+   </div>
+)};
+render(); {
+   const { timeSinceMount } = this.state;
+   const timeInterval = (new Date() - timeSinceMount) / 1000; // convert to seconds
+   return <div>Time since mount: {timeInterval} seconds</div>;
+ }
+ componentDidMount() {
+   this.intervalId = setInterval(() => {
+     this.setState({ timeSinceMount: new Date() })
+   }, 1000); // update every second
+ }
  
- function bio (props) {
-  return <h1>bio: {props.bio}</h1>;
+ componentWillUnmount() {
+   clearInterval(this.intervalId);
  }
- const App = () => {
-  return (
-    <div>
-      <bio bio="i love football" />
-      <bio bio="I am the King" /> 
-    </div>
-  );
- };
- function profession(props) {
-  return <h1>profession :{props.profession}</h1>;
- }
- 
- const profession = () => {
-  return (
-    <div>
-      <profession profession="psycologue" /> {/* 💥 "world" is the prop value*/}
-      <profession profession="doctor" /> {/* 💥 "I am the King" is the prop value*/}
-    </div>
-  );
-  
-);
-export default App;
-import PropTypes from "prop-types";
-Profilecomponent.propTypes = {
-  stringProp: PropTypes.string,
- };
